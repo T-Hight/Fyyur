c84fa030ef9a67d413f10142cf83f286f267ee49
T-Hight <taylorthight@gmail.com>
2023-06-01 07:28:06 -0800

    enums

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms.validators import DataRequired, URL
 import re
 from enums import Genre, State
 
