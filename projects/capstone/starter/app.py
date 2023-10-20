f6fe68c07f4058676e741ba28b4d8a514e7a169c
T-Hight <taylorthight@gmail.com>
2023-10-20 11:51:33 -0800

    first endpoint

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -3,14 +3,25 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from models import setup_db, Movies, Actors, db
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
-  CORS(app)
+  CORS(app, resources={"/": {"origins": "*"}})
+
+  #if db_URI:
+      #setup_db(app,db_URI)
+  #else:
+      #setup_db(app)
+
+  @app.route('/hello')
+  def hello():
+    return 'Hello, World!'
 
   return app
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(host='localhost', port=8080, debug=True)
