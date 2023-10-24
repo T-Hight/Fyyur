a6f7073638a12fac2c3a5c599f593f7eef2515cc
T-Hight <taylorthight@gmail.com>
2023-10-24 07:16:30 -0800

    auth

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,24 +2,37 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db, Movies, Actors
+from datetime import datetime
 
-from models import setup_db, Movies, Actors, db
+
+#from auth.auth import AuthError, requires_auth
+
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app, resources={"/": {"origins": "*"}})
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
 
-  #if db_URI:
-      #setup_db(app,db_URI)
-  #else:
-      #setup_db(app)
+    CORS(app)
 
-  @app.route('/')
-  def hello():
-    return 'Hello, World!'
+    @app.after_request
+    def after_request(response):
+      response.headers.add(
+          'Access-Control-Allow-Headers',
+          'Content-Type,Authorization,true')
+      response.headers.add(
+          'Access-Control-Allow-Methods',
+          'GET,PUT,POST,DELETE,OPTIONS')
+      return response
 
-  return app
+
+    @app.route('/')
+    def hello():
+      return 'Hello, World!'
+
+    return app
 
 APP = create_app()
 
