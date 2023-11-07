2044a3320675b2e6ce01ee22d25ef62f311e070c
T-Hight <taylorthight@gmail.com>
2023-11-07 07:48:05 -0900

    Create environment variables

--- projects/capstone/starter/setup.sh
+++ projects/capstone/starter/setup.sh
@@ -1,1 +1,7 @@
-
+export DATABASE_URL="postgresql+psycopg2://postgres:postgres@localhost:5432/Casting"
+export AUTH0_DOMAIN="taylorfsnd.us.auth0.com"
+export ALGORITHMS=["RS256"]
+export API_AUDIENCE="Casting"
+export ASSISTANT_TOKEN="Bearer "
+export DIRECTOR_TOKEN="Bearer "
+export PRODUCER_TOKEN="Bearer "
