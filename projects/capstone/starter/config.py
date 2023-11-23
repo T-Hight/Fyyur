2f0c0f1f5a731bc4da377ec871c127fade04766d
T-Hight <taylorthight@gmail.com>
2023-11-22 18:49:33 -0900

    Database URL

--- projects/capstone/starter/config.py
+++ projects/capstone/starter/config.py
@@ -7,7 +7,7 @@ class DBConfig:
     DEBUG = True
     SECRET_KEY = os.getenv('AUTH0_SECRET')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = ('postgres://casting_gr1i_user:5ba9UGeWI7W3xJ8nvsY2k0k0yptZZiDa@dpg-clf51pkp3ifc7390l630-a/casting_gr1i')
 
 class Auth0Config:
     domain = os.getenv('AUTH0_DOMAIN')
